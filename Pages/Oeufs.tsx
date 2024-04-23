9cb54516e45bbbf32936e0dbf40fd4be6bc926e1
Antoine <23antoine03@gmail.com>
2024-04-23 12:23:34 +0200

    Couleurs dégradées
    Ajout de fonctions de dégradé, conversion de couleurs
    + nombre de jours en fonction du mois
    + texte du jour sélectionné

--- Pages/Oeufs.tsx
+++ Pages/Oeufs.tsx
@@ -1,38 +1,120 @@
-import { StyleSheet, Text, View } from "react-native";
+import { PlatformColor, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import * as Dim from '../Utils/Dimensions';
+import * as Couleur from '../Utils/Couleurs';
+import { useState } from "react";
+
+// Paramètres
+
+const JOURS_MOIS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const NOMS_MOIS = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+const moisSelectionne = 1;
+
+const taille_disque = Dim.scale(5);
+const nb_disques = JOURS_MOIS[moisSelectionne];
+
+const couleur_debut_hex = Couleur.hexToRgb('#FFB9B9');
+const couleur_fin_hex = Couleur.hexToRgb('#C8B9FF');
+
+const couleur_debut_hex2 = Couleur.hexToRgb('#FF4C4C');
+const couleur_fin_hex2 = Couleur.hexToRgb('#7651FF');
+
+const couleur_debut =  couleur_debut_hex ? [couleur_debut_hex.r, couleur_debut_hex.g, couleur_debut_hex.b] : [0, 0, 0];
+const couleur_fin = couleur_fin_hex ? [couleur_fin_hex?.r, couleur_fin_hex?.g, couleur_fin_hex?.b] : [0, 0, 0];
+
+const couleur_debut2 =  couleur_debut_hex2 ? [couleur_debut_hex2.r, couleur_debut_hex2.g, couleur_debut_hex2.b] : [0, 0, 0];
+const couleur_fin2 = couleur_fin_hex2 ? [couleur_fin_hex2.r, couleur_fin_hex2.g, couleur_fin_hex2.b] : [0, 0, 0];
+
+const gradient = Couleur.degradeCouleur(couleur_debut, couleur_fin, nb_disques);
+const gradient2 = Couleur.degradeCouleur(couleur_debut2, couleur_fin2, nb_disques);
+
+function getRGBColorFromGradient(gradient: Array<Array<number>>, pos: number): string {
+    const color = 'rgb(' + gradient[0][pos] + ', ' + gradient[1][pos] + ', ' + gradient[2][pos] + ')';
+    return color;
+}
 
 /**
  * Page principale où se trouve la roue des jours
  */
 export default function Oeufs() {
+
+    const [jourSelectionne, setJourSelectionne] = useState(0);
+
     return (
-        <>
+        <View style={styles.wrapper}>
+
+            <Text style={[styles.affichageJour, {color: getRGBColorFromGradient(gradient2, jourSelectionne)}]}>{jourSelectionne+1} {NOMS_MOIS[moisSelectionne]}</Text>
+
             {
-                [...Array(100).keys()].map((i: number) => {
+                [...Array(nb_disques).keys()].map((i: number) => {
+
+                    const angle = i * 2 * Math.PI / nb_disques;
+                    const posX = Dim.widthScale(50) + Math.cos(angle) * Dim.scale(45) - taille_disque / 2;
+                    const posY = Dim.heightScale(50) + Math.sin(angle) * Dim.scale(45) - taille_disque / 2;  
+
+                    const color = getRGBColorFromGradient(gradient, i);
+
                     return (
-                        <Jour key={i} posx={i} posy={200} />
+                        <Jour key={i} posx={posX} posy={posY} couleur={color} id={i} onPress={(id: number) => setJourSelectionne(id)} selected={i == jourSelectionne } />
                     )
                 })
             }
-        </>
+        </View>
     )
 }
 
-function Jour({posx, posy}: {posx: number, posy: number}) {
+function Jour({posx, posy, style, couleur, id, onPress, selected}: {posx: number, posy: number, style?: Object, couleur: string, id: number, onPress: Function, selected: Boolean}) {
+
+    const [c, setC] = useState(couleur);
+
+    if(selected) {
+        posx += taille_disque / 4
+        posy += taille_disque / 4
+    }
 
     return (
-        <View style={[styles.disqueJour, {left: posx, bottom: posy}]}>
+        <TouchableOpacity
+            activeOpacity={0.8}
+            style={[styles.disqueJour, {left: posx, bottom: posy}, style, {backgroundColor: c}, selected ? styles.selected : styles.notSelected]}
+            onPress={() => {
+                const color = getRGBColorFromGradient(gradient2, id);
+                setC(color);
+                onPress(id);
+            }}>
             
-        </View>
+        </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     disqueJour: {
         position: 'absolute',
-        height: 10,
-        width: 10,
-        borderRadius: 5,
-
         backgroundColor: 'blue'
+    },
+    selected: {
+        height: taille_disque / 2,
+        width: taille_disque / 2,
+        borderRadius: taille_disque / 4
+    },
+    notSelected: {
+        height: taille_disque,
+        width: taille_disque,
+        borderRadius: taille_disque / 2
+    },
+    affichageJour: {
+        position: 'absolute',
+        top: Dim.heightScale(5),
+        width: Dim.widthScale(100),
+        left: 0,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontSize: Dim.scale(6),
+        fontWeight: 'bold'
+    },
+    wrapper: {
+        width: Dim.widthScale(100),
+        height: Dim.heightScale(100),
+        display: 'flex',
+        flex: 1,
+        backgroundColor: 'white'
     }
 });
---
+++ Utils/Couleurs.tsx
@@ -0,0 +1,57 @@
+/**
+ * Parameters
+ * ----------
+ * bornes : [debut, fin] -> Début et fin du domaine de dégradé
+ * N : nombre de valeurs à calculer entre début et fin
+ * 
+ * Returns
+ * -------
+ * d : Tableau de N valeurs dégradées linéairement entre début et fin
+ */
+function degradeTableau(bornes: Array<number>, N: number) {
+    const d = Array<number>(N);
+
+    const min = Math.min(bornes[0], bornes[1]);
+    const max = Math.max(bornes[0], bornes[1]);
+
+    for(var i = 0; i < N; ++i) {
+        d[i] = min + i * (max - min) / N;
+    }
+
+    if (bornes[0] < bornes[1]) {
+        return d;
+    }
+    else {
+        return d.reverse();
+    }
+}
+
+/**
+ * Parameters
+ * ----------
+ * rgbDebut : Couleur rgb de début de dégradé
+ * rgbFin : Couleur rgb de fin de dégradé
+ * N : nombre de couleurs intermédiaires à calculer
+ * 
+ * Returns
+ * -------
+ * gradient : [r, g, b] -> gradient[0][1] est la 2e valeur du rouge, gradient[2, 5] la 6e du bleu 
+ */
+function degradeCouleur(rgbDebut: Array<number>, rgbFin: Array<number>, N: number): Array<Array<number>> {
+    const r = degradeTableau([rgbDebut[0], rgbFin[0]], N);
+    const g = degradeTableau([rgbDebut[1], rgbFin[1]], N);
+    const b = degradeTableau([rgbDebut[2], rgbFin[2]], N);
+
+    return [r, g, b]
+}
+
+function hexToRgb(hex: string) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
+export {degradeTableau, degradeCouleur, hexToRgb};
