dc5ed9298dad417b4bf6c520fb196d82c6135cfe
Antoine <23antoine03@gmail.com>
2024-05-06 17:38:37 +0200

    Inscription / connexion
    Possibilité de s'inscrire et de se connecter
    Ajout du pseudonyme

--- Pages/Parametres.tsx
+++ Pages/Parametres.tsx
@@ -1,9 +1,9 @@
-import { Animated, ImageBackground, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native";
+import { Alert, Animated, ImageBackground, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native";
 import * as Dim from '../Utils/Dimensions';
 import { StackParamList } from "../App";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect, useRef, useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { User, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { auth } from '../firebase';
 import { TextInput } from "react-native-gesture-handler";
 
@@ -11,26 +11,115 @@ type Props = NativeStackScreenProps<StackParamList, 'Parametres'>;
 
 export default function Parametres({route, navigation}: Props) {
 
-    const [initializing, setInitializing] = useState(false);
-    const [user, setUser] = useState()
+    const [initializing, setInitializing] = useState(false)
+    const [user, setUser] = useState<User>()
+
+    const [warnMessages, setWarnMessages] = useState({email: '', password: ''})
+    const [inscription, setInscription] = useState(false)
 
     const email = useRef("");
     const password = useRef("");
+    const name = useRef("");
 
-    /*createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-        const u = userCredential.user;
-        console.log('User created : ' + u.email);
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log('User connected : ')
+            console.log(user.email)
+            console.log(user.displayName)
+            setUser(user)
+        } else {
+            setUser(undefined)
+        }
+    })
 
-        console.error('ERREUR : ' + errorCode + ' ' + errorMessage);
-    })*/
+    function createNewUser() {
+        console.log('Création d\'un nouveau compte...')
+        createUserWithEmailAndPassword(auth, email.current, password.current).then((userCredential) => {
+            const u = userCredential.user;
+            console.log('User created : ' + u.email);
+            updateProfile(u, {
+                displayName: name.current
+            }).then(() => {
+                console.log('Name updated !')
+                setUser(u)
+            }).catch((error) => {
+                console.error('ERREUR : ' + error.code + ' ' + error.message)
+            })
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+    
+            console.log('INSCRIPTION : ' + errorCode + ' ' + errorMessage);
+        })
+    }
 
-    if (initializing)
+    function connectUser() {
+        signInWithEmailAndPassword(auth, email.current, password.current).then((userCredential) => {
+            const u = userCredential.user;
+            console.log('User signed in : ' + u.email);
+            setUser(u)
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            console.log('CONNEXION FAILED : ' + errorCode + ' ' + errorMessage);
+
+            switch(errorCode) {
+                case 'auth/user-not-found':
+                    alertCreateNewAccount()
+                    return
+                case 'auth/wrong-password':
+                    console.log('WRONG PASSWORD')
+                    setWarnMessages({email: '', password: 'Mot de passe incorrect'})
+                    return
+                case 'auth/invalid-email':
+                    console.log('INVALID EMAIL')
+                    setWarnMessages({email: 'Adresse email invalide', password: ''})
+                    return
+                case 'auth/too-many-requests':
+                    Alert.alert('Trop de tentatives\nRéessayez plus tard.')
+                    return
+                default:
+                    return
+            }
+        })
+    }
+
+    /* Boite de dialogue : créer un nouveau compte */
+
+    function alertCreateNewAccount() {
+        Alert.alert(
+            '',
+            'Aucun compte n\'est associé à cet email, souhaitez-vous en créer un ?',
+            [
+                {
+                    text: 'Non',
+                    onPress: () => {
+
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: 'Oui',
+                    onPress: () => {
+                        setInscription(true)
+                    },
+                    style: 'default'
+                }
+            ],
+            {
+                cancelable: true,
+                onDismiss: () => {
+                    
+                }
+            }
+        )
+    }
+
+    if (initializing) // Chargement de la page
         return null
 
-    if (!user) {
+    if (!user) { // Utilisateur déconnecté
         return (
             <View style={styles.wrapper}>
                 <TouchableOpacity style={styles.invisible} activeOpacity={1} onPress={() => {
@@ -43,46 +132,64 @@ export default function Parametres({route, navigation}: Props) {
                     <TouchableOpacity style={styles.retourWrapper} activeOpacity={0.8} onPress={() => {
                         navigation.goBack();
                     }}>
-                    <Text style={styles.retourTexte}>Retour</Text>
-                </TouchableOpacity>
-
-                <View style={styles.connexion}>
-
-                    <Text style={styles.connexionTitle}>Connexion</Text>
-
-                    <InputField
-                        title="Adresse email"
-                        onSubmit={(text: string) => {
-                            email.current = text
-                        }}
-                    />
-
-                    <InputField
-                        title="Mot de passe"
-                        password={true}
-                        onSubmit={(text: string) => {
-                            password.current = text
-                        }}
-                    />
-
-                    <TouchableOpacity
-                        style={styles.connexionButton}
-                        activeOpacity={0.8}
-                        onPress={() => {
-                            console.log('Adresse mail : ' + email.current)
-                            console.log('Mot de passe : ' + password.current)
-                        }}
-                    >
-                        <Text style={styles.connexionButtonText}>Valider</Text>
+                        <Text style={styles.retourTexte}>Retour</Text>
                     </TouchableOpacity>
 
+                    { /* Page de connexion */}
+
+                    <View style={styles.connexion}>
+
+                        <Text style={styles.connexionTitle}>Connexion</Text>
+
+                        <InputField
+                            title="Adresse email"
+                            onSubmit={(text: string) => {
+                                email.current = text
+                            }}
+                            footer={warnMessages.email}
+                            footerColor='red'
+                        />
+
+                        <InputField
+                            title="Pseudo"
+                            onSubmit={(text: string) => {
+                                name.current = text
+                            }}
+                            visible={inscription}
+                        />
+
+                        <InputField
+                            title="Mot de passe"
+                            password={true}
+                            onSubmit={(text: string) => {
+                                password.current = text
+                            }}
+                            footer={warnMessages.password}
+                            footerColor='red'
+                        />
+
+                        {/* Connexion à un utilisateur existant */}
+
+                        <TouchableOpacity
+                            style={styles.connexionButton}
+                            activeOpacity={0.8}
+                            onPress={() => {
+                                console.log('Adresse mail : ' + email.current)
+                                console.log('Mot de passe : ' + password.current)
+
+                                if (inscription) createNewUser()
+                                else connectUser()
+                            }}
+                        >
+                            <Text style={styles.connexionButtonText}>{inscription ? 'S\'inscrire' : 'Se connecter'}</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </View>
         )
     }
 
-    return (
+    return ( // Utilisateur connecté
         <View style={styles.wrapper}>
             <TouchableOpacity style={styles.invisible} activeOpacity={1} onPress={() => {
                 navigation.goBack();
@@ -97,33 +204,58 @@ export default function Parametres({route, navigation}: Props) {
                 </TouchableOpacity>
 
                 <Text>Signed up</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        console.log('Déconnexion...')
+                        signOut(auth).then(() => {
+                            console.log('Utilisateur déconnecté.')
+                        }).catch((error) => {
+                            console.error('ERREUR : ' + error)
+                        })
+                    }}
+                >
+                    <Text>Se déconnecter</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
 }
 
-function InputField({title, password=false, onSubmit}: {title: string, password?: Boolean, onSubmit: Function}) {
+function InputField({title, password=false, onSubmit, footer='', footerColor='black', visible=true}: {title: string, password?: Boolean, onSubmit: Function, footer?: string, footerColor?: string, visible?: Boolean}) {
 
     const text = useRef("")
 
+    if (!visible)
+        return null
+
     return (
-        <View style={styles.fieldWrapper}>
-            <Text style={styles.textField}>
-                {title}
-            </Text>
-            <TextInput
-                style={styles.inputField}
-                secureTextEntry={password ? true : false}
-                onChangeText={(t: string) => {
-                    text.current = t
-                }}
-                onBlur={() => {
-                    onSubmit(text.current);
-                }}
-            />
+        <View>
+            <View style={styles.fieldWrapper}>
+                <Text style={styles.textField}>
+                    {title}
+                </Text>
+                <TextInput
+                    style={styles.inputField}
+                    secureTextEntry={password ? true : false}
+                    onChangeText={(t: string) => {
+                        text.current = t
+                    }}
+                    onBlur={() => {
+                        onSubmit(text.current);
+                    }}
+                />
+            </View>
+            <Text style={[inputStyle.footer, {color: footerColor}]}>{footer}</Text>
         </View>
     )
 }
+
+const inputStyle = StyleSheet.create({
+    footer: {
+        textAlign: 'left',
+        fontSize: Dim.scale(3),
+    }
+})
 
 const styles = StyleSheet.create({
     wrapper: {
